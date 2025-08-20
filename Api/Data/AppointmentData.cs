4acfe8f46cdf3870b85271d2535000a4f37eb0a7
Jonathan Royle <jroyle@neurofenix.com>
2025-08-20 10:55:38 +0200

    Add list appointments endpoint

--- Api/Data/AppointmentData.cs
+++ Api/Data/AppointmentData.cs
@@ -13,7 +13,9 @@ internal static class AppointmentData
             CustomerId = AnimalData.Animals.First().OwnerId,
             StartTime = DateTime.Now.AddDays(1),
             EndTime = DateTime.Now.AddDays(1).AddHours(1),
-            Notes = "Vet appointment"
+            Notes = "Vet appointment",
+            VeterinarianId = new Guid("70a5e606-25c9-4412-907d-e490bee7bfce"),
+            Status = AppointmentStatus.Scheduled
         },
         new Appointment
         {
@@ -21,6 +23,21 @@ internal static class AppointmentData
             AnimalId = AnimalData.Animals.First().Id,
             CustomerId = AnimalData.Animals.First().OwnerId,
             Notes = "Follow-up check",
+            StartTime = DateTime.Now.AddDays(2),
+            EndTime = DateTime.Now.AddDays(2).AddHours(1),
+            VeterinarianId = new Guid("70a5e606-25c9-4412-907d-e490bee7bfce"),
+            Status = AppointmentStatus.Scheduled
+        },
+        new Appointment
+        {
+            Id = new Guid("f47ac10b-58cc-4372-a567-0e02b2c3d481"),
+            AnimalId = new Guid("f47ac10b-58cc-4372-a567-0e02b2c3d477"),
+            CustomerId = AnimalData.Animals.FirstOrDefault(a => a.Id == new Guid("f47ac10b-58cc-4372-a567-0e02b2c3d477")).OwnerId,
+            Notes = "Follow-up check",
+            StartTime = DateTime.Now.AddDays(2),
+            EndTime = DateTime.Now.AddDays(2).AddHours(1),
+            VeterinarianId = new Guid("7399388f-3134-4381-951a-de10a9895bd0"),
+            Status = AppointmentStatus.Scheduled
         },
     };
 }
